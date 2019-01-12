fde25be6a6cd0fe42507301e4e22ae3bb3db843d
marcell <mvc@linux.com>
2019-01-12 00:40:13 +0000

    scaffold API

--- logical_lens/logical_lens.py
+++ logical_lens/logical_lens.py
@@ -11,3 +11,15 @@ LENS = Mapping[DATA, Mapping[Sequence[float], bool]]
 class LogicalLens:
     n: int = attr.ib()  # TODO: assert n >= 1
     lens: LENS
+
+    def dist(self, data1, data2):
+        raise NotImplementedError
+
+    def adj_matrix(self, data):
+        raise NotImplementedError
+
+    def project(self, indices, data, *, as_percent=True):
+        raise NotImplementedError
+
+    def random_project(data, n):
+        raise NotImplementedError
