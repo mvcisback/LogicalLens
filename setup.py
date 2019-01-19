79b028a53a3887cea2a43ab5139580fd682fb690
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-01-19 15:30:57 -0800

    add description in readme

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='logical-lens',
-    version='0.0.0',
-    description='TODO',
+    version='0.0.1',
+    description='Python library for using parametric specifications to embed domain specific knowledge in machine learning.',
     url='https://github.com/mvcisback/LogicalLens',
     author='Marcell Vazquez-Chanlatte',
     author_email='marcell.vc@eecs.berkeley.edu',
